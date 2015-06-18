6c19998258b1f7f126723acc844db9b6ad49f4df
Stephan Bosch <stephanmbosch@gmail.com>
2015-06-17 19:45:42 -0700

    not sure

--- hw/hw4/break_me.py
+++ hw/hw4/break_me.py
@@ -1,9 +1,16 @@
 def exhibit_name_error():
     print(foo)
 
+
 def exhibit_type_error():
     print(3 + "a")
 
+
 def exhibit_attribute_error():
-    some_num = 42
-    print(some_num.append(55))
+    some_num = 43
+    print(some_num.append(32))
+
+
+exhibit_name_error()
+exhibit_type_error()
+exhibit_attribute_error()
