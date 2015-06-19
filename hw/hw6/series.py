20f0c32e3d6c54b1500a86b4faff9cff7c571b26
Stephan Bosch <stephanmbosch@gmail.com>
2015-06-18 20:32:21 -0700

    started working on lucas series, hit a road block

--- hw/hw6/series.py
+++ hw/hw6/series.py
@@ -9,3 +9,18 @@ num_range = int(input("How many terms shall we evaluate? "))
 
 for i in range(num_range):
     print(fibonacci(i))
+
+
+def lucas(n):
+    l.append(2)
+    l.append(1)
+    if l <= 2:
+        return l
+    else:
+        return(lucas(l - 1) + lucas(l - 2))
+
+
+num_range2 = int(input("How many terms shall we evaluate? "))
+
+for i in range(num_range2):
+    print(lucas(i))
