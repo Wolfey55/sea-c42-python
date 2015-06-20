fa34f175bda04d9cb720de765d59ed092cb18d1e
Stephan Bosch <stephanmbosch@gmail.com>
2015-06-20 12:34:25 -0700

    wrote sum_series function but cant call it with different parameters other than default

--- hw/hw6/series.py
+++ hw/hw6/series.py
@@ -12,7 +12,7 @@ for i in range(num_range):
 
 
 def lucas(n):
-    # function to print lucas series
+    # function to evaluate lucas series
     if n < 1:
         return 2
     if n == 1:
@@ -26,3 +26,30 @@ num_range2 = int(input("How many terms shall we evaluate? "))
 # print the range set by the user
 for i in range(num_range2):
     print(lucas(i))
+
+
+# function to evaluate both fibonacci and lucas series
+def sum_series(n, a=0, b=1):
+    # evaluates fibonacci series
+    if a == 0 and b == 1:
+        if n <= 1:
+            return n
+        else:
+            return(sum_series(n - 1) + sum_series(n - 2))
+
+    # evaluates lucas series
+    elif a == 2 and b == 1:
+        if n < 1:
+            return 2
+        if n == 1:
+            return n
+        else:
+            return(sum_series(n - 1) + sum_series(n - 2))
+
+# input from user
+num_range3 = int(input("How many terms shall we evaluate? "))
+
+# prints the range set by user
+for i in range(num_range3):
+    print(sum_series(i))
+
