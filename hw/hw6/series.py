4c174a7a1a240d2ed2550fcada6ccc491d46fdd4
Stephan Bosch <stephanmbosch@gmail.com>
2015-06-22 19:42:29 -0700

    hw6 completed, had to rewrite sum_series()

--- hw/hw6/series.py
+++ hw/hw6/series.py
@@ -4,11 +4,7 @@ def fibonacci(n):
     else:
         return(fibonacci(n - 1) + fibonacci(n - 2))
 
-
-num_range = int(input("How many terms shall we evaluate? "))
-
-for i in range(num_range):
-    print(fibonacci(i))
+print(fibonacci(0))
 
 
 def lucas(n):
@@ -20,36 +16,17 @@ def lucas(n):
     else:
         return(lucas(n - 1) + lucas(n - 2))
 
-# input from user
-num_range2 = int(input("How many terms shall we evaluate? "))
-
-# print the range set by the user
-for i in range(num_range2):
-    print(lucas(i))
+print(lucas(0))
 
 
 # function to evaluate both fibonacci and lucas series
 def sum_series(n, a=0, b=1):
     # evaluates fibonacci series
-    if a == 0 and b == 1:
-        if n <= 1:
-            return n
-        else:
-            return(sum_series(n - 1) + sum_series(n - 2))
+    if n < 1:
+        return a
+    elif n == 1:
+        return b
+    else:
+        return(sum_series(n - 1, a, b) + sum_series(n - 2, a, b))
 
-    # evaluates lucas series
-    elif a == 2 and b == 1:
-        if n < 1:
-            return 2
-        if n == 1:
-            return n
-        else:
-            return(sum_series(n - 1) + sum_series(n - 2))
-
-# input from user
-num_range3 = int(input("How many terms shall we evaluate? "))
-
-# prints the range set by user
-for i in range(num_range3):
-    print(sum_series(i))
 
