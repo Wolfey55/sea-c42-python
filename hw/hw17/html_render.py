b4a3b79902cc00728137d234ab7a7d84813225bb
Stephan Bosch <stephanmbosch@gmail.com>
2015-07-09 18:12:42 -0700

    problem 2 complete, fixed run_html_output.py

--- hw/hw17/html_render.py
+++ hw/hw17/html_render.py
@@ -11,18 +11,30 @@ Python class example.
 
 class Element(object):
 
-    indent = "    "
+    INDENT = "    "
 
     def __init__(self, tag='', content=''):
         self.tag = tag
-        self.content = content
+        self.children = [content] if content else []
 
-    def append(self, new_text):
-        self.content += new_text
+    def append(self, new_child):
+        self.children.append(new_child)
 
-    def render(self, file_out, indent=""):
-        indented_content = indent + self.content
-        file_out.write(indented_content)
+    def render(self, file_out, indent="    "):
+        file_out.write('%s<%s>\n' % (indent, self.tag))
+        for child in self.children:
+            if (type(child) == str):
+                # add new content string without rendering
+                file_out.write(indent + Element.INDENT + child + '\n')
+
+            else:
+                # add new child node by recursively rendering
+                child.render(file_out, indent + Element.INDENT)
+        file_out.write('%s</%s>\n' % (indent, self.tag))
+
+        #indented_content = indent + Element.INDENT + self.content
+        #final_content = '<%s>\n %s \n</%s>' % (self.tag, indented_content, self.tag)
+        #file_out.write(final_content)
 
 
 class Html(Element):
@@ -30,14 +42,19 @@ class Html(Element):
     def __init__(self):
         Element.__init__(self, 'html', '')
 
+    def render(self, file_out, indent=''):
+        file_out.write('<!DOCTYPE html>\n')
+        Element.render(self, file_out, '')
+
 
 class Body(Element):
 
-    def __init__(self):
+    def __init__(self, content=''):
         Element.__init__(self, 'body', '')
 
 
 class P(Element):
 
-    def __init__(self, content):
+    def __init__(self, content=''):
         Element.__init__(self, 'p', content)
+
