d33a3e6e95c638bccc25e359e42728fb44748b4d
Stephan Bosch <stephanmbosch@gmail.com>
2015-07-11 20:41:59 -0700

    Assignment complete!

--- hw/hw13/trigrams.py
+++ hw/hw13/trigrams.py
@@ -3,8 +3,9 @@ import string
 import random
 import io
 
+# file to be imported
 filename = "sherlock_small.txt"
-d = {}
+
 
 def read_data(file_name):
 
@@ -20,7 +21,6 @@ def read_data(file_name):
             full_text.append(lines[line])
 
         # Joins all the items in full_text to create one string.
-        print(" ".join(full_text))
         return " ".join(full_text)
 
     except IOError as e:
@@ -30,42 +30,107 @@ def read_data(file_name):
 
 def strip_punctuation(texts):
 
-    punctuation = string.punctuation
-    punctuation = string.punctuation.replace("'", "")  # keep apostropies
-    punctuation = string.punctuation.replace("-", "")  # keep single hyphon
-    table = dict([(ord(c), None) for c in punctuation])
+    # function to evaluate whether characters in texts are punctuation.
+    def is_punctuation(char):
 
-    # replace all the '--' with a space
-    texts = texts.replace('--', ' ')
+        # if char is an apostrophy return False
+        if char is "'":
+            return 0
+        # if char is a hyphon retrun False
+        elif char is '-':
+            return 0
+        # if char is any other punctuation retrun True
+        elif char in string.punctuation:
+            return 1
+        # any number or letter return False
+        else:
+            return 0
+
+    stripped_text = ''
+    # iterate through every character in texts.
+    for char in texts:
+        # if is_punctuation function returns False for said character:
+        if is_punctuation(char) is 0:
+            # add character to new text
+            stripped_text += char
 
     # lower-case everything to remove that complication:
-    texts = texts.lower()
+    stripped_text = stripped_text.lower()
+
+    # replace all the '--' and '-' with a space
+    stripped_text = stripped_text.replace('--', ' ')
+    stripped_text = stripped_text.replace('-', ' ')
 
     # remove punctuation
-    texts = texts.translate(table)
+    # texts = texts.translate(table)
 
     # split into words
-    words = texts.split()
-    # words = texts.split("--")
+    words = stripped_text.split()
 
     # remove the bare single quotes
     # " ' " is both a quote and an apostrophe
     words = [word for word in words if word != "'"]
 
-    print(words)
     return words
 
-    #for (i, word) in enumerate(words):
-        #d[(words[i], words[i + 1])] = [words[i + 2]]
-    #print(d)
 
+def trigram_constructor(words):
+
+    word_pairs = {}
+
+    # iterate through words stopping short of the end to prevent error
+    for w in range(len(words) - 2):
+        # takes the current index and following index and makes a tuple
+        # this will be used as word_pairs key
+        leading_pair = tuple(words[w:w+2])
+        # assigns the word following the tuple
+        # this will be used as the word_pairs value
+        follower = words[w+2]
+        # if the leading pair is already in word_pairs:
+        if leading_pair in word_pairs:
+            # append the 'follower' word in the value list
+            word_pairs[leading_pair].append(follower)
+        # If leading pair is not in word_pairs:
+        else:
+            # create new tuple(key):[value]
+            word_pairs[leading_pair] = [follower]
+
+    return word_pairs
+
+
+def create_text(word_pairs):
+
+    # create an empty list for the new story to be stored in.
+    new_story = []
+
+    for i in range(100):
+        # create list of the keys in word_pairs.
+        key_list = list(word_pairs.keys())
+        # select a random index of key_list to start the sentence.
+        sentence = list(random.choice(key_list))
+
+        for j in range(random.randint(2, 10)):
+            # create new word_pairs[key] from the last two words in sentence.
+            pair = tuple(sentence[-2:])
+            if pair in word_pairs:
+                # append a randomly chosen value for matching key
+                sentence.append(random.choice(list(word_pairs[pair])))
+            else:
+                # if the pair doesn't exist, stop adding words and end sentence.
+                break
+        # capitalize beginning of sentence
+        sentence[0] = sentence[0].capitalize()
+        # add period at the end of sentence
+        sentence[-1] += '.'
+        new_story.extend(sentence)
+
+    new_story = " ".join(new_story)
+
+    print(new_story)
+    return new_story
 
 if __name__ == "__main__":
 
-    read_data(filename)
+    # strip_punctuation(read_data(filename))
 
-    strip_punctuation(read_data(filename))
-
-
-
-
+    create_text(trigram_constructor(strip_punctuation(read_data(filename))))
